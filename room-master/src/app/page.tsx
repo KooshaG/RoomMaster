255bcc9c391418fcbc3502c9b16837ba8f61806d
KooshaG <75464454+KooshaG@users.noreply.github.com>
2023-06-19 01:07:02 -0400

    another attempt

--- room-master/src/app/page.tsx
+++ room-master/src/app/page.tsx
@@ -3,7 +3,7 @@ import HeroBoxes from '@/components/HeroBoxes';
 export default async function Home() {
 
   const res = await fetch(`https://${process.env.VERCEL_URL}/api/stats?search=reservations`, {next: { revalidate: 300}});
-  const data: {count: number} = await res.json() || 0; 
+  const data: {count: number} = await res.json().catch(() => console.log("error happened")) || 0; 
 
   return (
     <>
