268c64bff2b9f270b9ec2072d39fbc8a5882c9df
KooshaG <75464454+KooshaG@users.noreply.github.com>
2024-01-20 01:14:26 -0500

    badge fix

--- room-master/src/components/Reservation/ReservationEntry.tsx
+++ room-master/src/components/Reservation/ReservationEntry.tsx
@@ -10,7 +10,7 @@ interface ReservationEntryProps {
 
 const dateDiff = (date1: Date, date2: Date) => {
   const diffTime = date2.getTime() - date1.getTime();
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
   return diffDays;
 };
 
